e551ebd369f9a3ae29d272bd830e49fd165711f1
Mouton <watel.dimitri@gmail.com>
2012-01-12 23:29:50 +0100

    Commentaires de cartes.py
    Signed-off-by: Mouton <watel.dimitri@gmail.com>

--- cartes.py
+++ cartes.py
@@ -1,11 +1,17 @@
 import operator
 
-# Test
+# Cette classe représente les cartes que les joueurs ont en main. 
+# Aussi bien les cartes de ressources que les cartes de développement. 
+# Elles sont représentées ensembles de la même façon pour la simple
+# raison qu'elles sont manipulées de la même façon.
+# Il s'agit d'un upplet d'entiers, chaque coordonnée correspond à
+# un type de carte.
 class CartesGeneral:
     attributs = ['argile', 'ble', 'bois', 'caillou', 'mouton', 'chevalliers',
                  'monopoles', 'pointsVictoire', 'decouverte',
                  'constructionDeRoute']
 
+	# Construit l'upplet de carte manuellement.
     def __init__(self, argile, ble, bois, caillou, mouton, chevalliers,
                  monopoles, pointsVictoire, decouverte, constructionDeRoute):
         self.argile = argile
@@ -19,59 +25,74 @@ class CartesGeneral:
         self.decouverte = decouverte
         self.constructionDeRoute = constructionDeRoute
 
+	# Renvoie vrai si l'opération appliquée à tout attribut de self et other renvoie vrai.
     def _comparison(self, other, operation):
         return all(operation(getattr(self, att), getattr(other, att))
             for att in CartesGeneral.attributs)
 
+	# Renvoie vrai si les deux mains sont identiques
     def __eq__(self,other):
         return self._comparison(other, operator.eq)
 
+	# Renvoie la fusion des deux mains
     def __add__(self,other):
         args = [getattr(self, att) + getattr(other, att)
                 for att in CartesGeneral.attributs]
         return CartesGeneral(*args)
 
+	# Multiplie chaque carte de la main par un nombre other
     def __mul__(self,other):
         args = [getattr(self, att) * other
                 for att in CartesGeneral.attributs]
         return CartesGeneral(*args)
 
+	# Renvoie la différence de la première main par la deuxième main
     def __sub__(self,other):
         return self + other*(-1)
 
+	# Renvoie le nombre de cartes en main
     def size(self):
         return sum(getattr(self, att) for att in CartesGeneral.attributs)
 
+	# Renvoie le nombre de ressources en main
     def ressources_size(self):
         return self.argile + self.ble + self.bois + self.caillou + self.mouton
 
+	# Renvoie vrai si la main de other a strictement plus de carte de chaque type que self
     def __lt__(self,other):
         return self._comparison(other, operator.lt)
 
+	# Renvoie vrai si la main de other a strictement moins de carte de chaque type que self. Ce n'est donc pas l'opération inverse de le
     def __gt__(self,other):
         return self._comparison(other, operator.gt)
 
+	# Renvoie vrai si la main de other a plus de carte de chaque type que self
     def __le__(self,other):
         return self._comparison(other, operator.le)
 
+	# Renvoie vrai si la main de other a moins de carte de chaque type que self. Ce n'est donc pas l'opération inverse de lt
     def __ge__(self,other):
         return self._comparison(other, operator.ge)
 
     def __str__(self):
         return '(' +  ', '.join(str(getattr(self, att)) for att in CartesGeneral.attributs) + ')'
 
+	# Renvoie vrai si la main ne contient que des ressources
     def est_ressource(self):
         return (self.chevalliers == 0 and self.pointsVictoire == 0
                 and self.decouverte == 0 and self.constructionDeRoute == 0
                 and self.monopoles == 0)
 
+	# Renvoie vrai si la main ne contient que des nombres entiers
     def est_entier(self):
         return all(isinstance(getattr(self, att), int)
             for att in CartesGeneral.attributs)
 
+	# Renvoie vrai si la main ne contient que des entiers positifs
     def est_physiquement_possible(self):
         return self >= Cartes.RIEN and self.est_entier()
 
+	# Renvoie la cartes numéro nb de la main, si elles sont classées dans l'ordre des attributs.
     def carte(self,nb):
         if 0 >= nb > self.ressources_size():
             return 0
@@ -102,6 +123,8 @@ class CartesGeneral:
             nb -= self.mouton
         return 0
 
+		
+	# Renvoie le nombre de carte en main du type désigné.
     def get_cartes_de_type(self,carte):
         if carte == Cartes.ARGILE:
             i = self.argile
@@ -129,7 +152,7 @@ class CartesGeneral:
 
 
 
-
+# Permet une petite facilité quand on ne travaille qu'avec des ressources
 class CartesRessources(CartesGeneral):
     def __init__(self,argile,ble,bois,caillou,mouton):
 	CartesGeneral.__init__(self,argile,ble,bois,caillou,mouton,0,0,0,0,0)
@@ -137,7 +160,7 @@ class CartesRessources(CartesGeneral):
     def __str__(self):
         return '(' + str(self.argile) +','+ str(self.ble) + ',' + str(self.bois) + ',' + str(self.caillou) + ',' + str(self.mouton) + ')'
 
-
+# Permet une petite facilité quand on ne travaille qu'avec des certes de développement
 class CartesDeveloppement(CartesGeneral):
     def __init__(self,chevalliers, monopoles, pointsVictoire, decouverte, constructionDeRoute):
         CartesGeneral.__init__(self,0,0,0,0,0,chevalliers, monopoles, pointsVictoire, decouverte, constructionDeRoute)
@@ -145,6 +168,7 @@ class CartesDeveloppement(CartesGeneral):
     def __str__(self):
         return '(' + str(self.chevalliers) +','+ str(self.monopoles) + ',' + str(self.pointsVictoire) + ',' + str(self.decouverte) + ',' + str(self.constructionDeRoute) + ')'
 
+# Vecteurs unitaires
 class Cartes:
     RIEN = CartesGeneral(0,0,0,0,0,0,0,0,0,0)
     ARGILE = CartesRessources(1,0,0,0,0)
