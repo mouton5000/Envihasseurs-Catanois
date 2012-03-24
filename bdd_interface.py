9f6fe2e6aa905061b6c62bc8b7138ac8b7afbe04
Dimitri <watel.dimitri@gmail.com>
2012-03-24 16:52:01 +0100

    Mise à jour des interfaces masquant et remplacant une partie de la base de donnée à l'aide d'une base de donnée virtuelle placée entre l'utilisateur et la bdd

--- bdd_interface.py
+++ bdd_interface.py
@@ -1,43 +1,153 @@
+# *-* coding: iso-8859-1 *-*
 import redis
+import copy
 
 REDIS = redis.StrictRedis()
-
 class BDD:
 
-    def __init__(self):
-        self.dict = {}
+    DELETE_TYPE = {'set' : set([]), 'list' : []}
 
+    def __init__(self, bdd = REDIS):
+        self.dict = {}
+        self.bdd = bdd
+
+    def type(self,key):
+        if key in self.dict:
+            return str(type(self.dict[key]))
+        else:
+            return self.bdd.type(key)
+        
+    
     def set(self,key,value):
         self.dict[key] = str(value)
 
     def get(self,key):
-        return self.dict[key]
+        if key in self.dict:
+            return self.dict[key]
+        else:
+            return self.bdd.get(key)
 
     def exists(self,key):
-        return key in self.dict
+        if key in self.dict:
+            return True
+        else:
+            return self.bdd.exists(key)
 
     def keys(self):
-        return self.dict.keys()
+        keys = self.bdd.keys()
+        return list(set(self.dict.keys() + keys))
+
+    def sismember(self,key,member):
+        if key in self.dict:
+            return str(member) in self.dict[key]
+        else:
+            return self.bdd.ismember(key,member)
+
+    def smembers(self,key):
+        if key in self.dict:
+            return self.dict[key]
+        else:
+            return self.bdd.smembers(key)
 
     def sadd(self,key,value):
-        if not self.exists(key):
-            self.dict[key] = set([])
+        if not key in self.dict or self.dict[key] == None:
+            self.dict[key] = copy.copy(self.smembers(key))
         self.dict[key].add(str(value))
 
     def srem(self,key,value):
-        if (self.exists(key)):
-            self.dict[key].remove(str(value))
+        if not key in self.dict or self.dict[key] == None:
+            self.dict[key] = copy.copy(self.smembers(key))
+        self.dict[key].remove(str(value))
+
+    def linsert(self,key,after,pivot,value):
+        if not key in self.dict:
+            self.dict[key] = self.bdd.lrange(key,0,-1)
+        a = self.dict[key]
+        i = a.index(str(pivot))
+        bafter = (after == 'after')
+        if bafter:
+            i += 1
+        # Si c'est after, on insère sur l'indice suivant, sinon sur l'indice précédent.
+        a.insert(i,str(value))
+
+    def llen(self, key):
+        if key in self.dict:
+            return len(self.dict[key])
+        return self.bdd.llen(key)
+        
+
+    def lindex(self, key, index):
+        if key in self.dict:
+            return self.dict[key][index]
+        return self.bdd.lindex(key, index)
+
+    def lpush(self, key, value):
+        if not key in self.dict:
+            self.dict[key] = self.bdd.lrange(key,0,-1)
+        self.dict[key].insert(0,str(value))
+
+    def lrange(self,key,i1,i2):
+        if key in self.dict:
+            a = self.dict[key]
+            return a[i1:i2] + [a[i2]]
+        else:
+            return self.bdd.lrange(key,i1,i2)
+        
+    def lrem(self,key,count, value):
+        if not key in self.dict:
+            self.dict[key] = self.bdd.lrange(key,0,-1)
+        a = self.dict[key]
+        svalue = str(value)
+        if count == 0:
+            while svalue in a:
+                a.remove(svalue)
+        else:
+            if count < 0:
+                a.reverse()
+            for i in xrange(0,abs(count)):
+                a.remove(svalue)
+            if count < 0:
+                a.reverse()
+
+    def rpush(self,key,value):
+        if not key in self.dict:
+            self.dict[key] = self.bdd.lrange(key,0,-1)
+        self.dict[key].append(str(value))
 
     def flushdb(self):
-        self.dist.clear()
+        ''' Supprime les clés de la base de donnée entière '''
+        self.bdd.flushdb()
+        self.flushSurface()
+    
+    def flushSurface(self):
+        ''' Ne supprime que les clés de la base de donné virtuelle '''
+        self.dict.clear()
+
+    def delete(self, key):
+        ''' Supprime cette clé de la base de donnée vitruelle, pour éviter des problèmes, cette clé est virtuellement supprimée : sa valeur est mise à None. On saura ainsi faire la différence entre une clé supprimée et une clé qui n'est pas utilisée dans la base virtuelle. '''
+        t = self.type(key)
+        if t in BDD.DELETE_TYPE.keys():
+            self.dict[key] = BDD.DELETE_TYPE[self.type(key)]
+        else:
+            self.dict[key] = None
 
     def save(self):
-        pipe = REDIS.pipeline()
-        for key in self.keys():
+        for key in self.dict.keys():
             v = self.dict[key]
             if isinstance(v,set):
+                self.bdd.delete(key)
                 for i in v:
-                    pipe.sadd(key,i)
+                    self.bdd.sadd(key,i)
+            elif isinstance(v,list):
+                self.bdd.delete(key)
+                for i in v:
+                    self.bdd.rpush(key,i)
             else:
-                pipe.set(key,v)
-        pipe.execute()
+                self.bdd.set(key,v)
+        self.flushSurface()
+
+
+    def saveAll(self):
+        self.save()
+        if self.bdd != REDIS:
+            self.bdd.saveAll()
---
+++ test_bdd.py
@@ -0,0 +1,230 @@
+# *-* coding: iso-8859-1 *-*
+import unittest
+from bdd_interface import *
+import redis
+
+REDIS = redis.StrictRedis()
+
+class TestForetAction(unittest.TestCase):
+    def setUp(self):
+        # On crée un arbre avec une racine vide, sans fils. Le curseur est placé sur
+        # la racine
+        self.b1 = BDD()
+        self.b2 = BDD(self.b1)
+        REDIS.flushdb()
+
+    def test_get_set(self):
+        b1 = self.b1
+        b2 = self.b2
+        r = REDIS
+
+        s1 = 'test1'
+        v1 = 1
+        sv1 = str(v1)
+
+        s2 = 'test2'
+        v2 = 2
+        sv2 = str(v2)
+
+        s3 = 'test3'
+        v3 = 3
+        sv3 = str(v3)
+
+        self.assertEqual(r.get(s1), None)
+        self.assertEqual(b1.get(s1), None)
+        self.assertEqual(b2.get(s1), None)
+
+        r.set(s1,v1)
+        self.assertEqual(r.get(s1), sv1)
+        self.assertEqual(b1.get(s1), sv1)
+        self.assertEqual(b2.get(s1), sv1)
+        
+        b1.set(s2,v2)
+        self.assertEqual(r.get(s2), None)
+        self.assertEqual(b1.get(s2), sv2)
+        self.assertEqual(b2.get(s2), sv2)
+        
+        b2.set(s3,v3)
+        self.assertEqual(r.get(s3), None)
+        self.assertEqual(b1.get(s3), None)
+        self.assertEqual(b2.get(s3), sv3)
+
+        b1.set(s1,v3)
+        self.assertEqual(r.get(s1), sv1)
+        self.assertEqual(b1.get(s1), sv3)
+        self.assertEqual(b2.get(s1), sv3)
+        
+        b2.set(s1,v2)
+        self.assertEqual(r.get(s1), sv1)
+        self.assertEqual(b1.get(s1), sv3)
+        self.assertEqual(b2.get(s1), sv2)
+
+        b1.save() 
+        self.assertEqual(r.get(s1), sv3)
+        self.assertEqual(b1.get(s1), sv3)
+        self.assertEqual(b2.get(s1), sv2)
+        
+        b2.save() 
+        self.assertEqual(r.get(s1), sv3)
+        self.assertEqual(b1.get(s1), sv2)
+        self.assertEqual(b2.get(s1), sv2)
+        
+        b2.saveAll() 
+        self.assertEqual(r.get(s1), sv2)
+        self.assertEqual(b1.get(s1), sv2)
+        self.assertEqual(b2.get(s1), sv2)
+
+    def test_set(self):
+        b1 = self.b1
+        b2 = self.b2
+        r = REDIS
+
+        s1 = 'test1'
+        v11 = 11
+        v12 = 12
+        v13 = 13
+        v14 = 14
+        sv11 = str(v11)
+        sv12 = str(v12)
+        sv13 = str(v13)
+        
+        self.assertEqual(r.smembers(s1), set([]))
+        self.assertEqual(b1.smembers(s1), set([]))
+        self.assertEqual(b2.smembers(s1), set([]))
+
+        r.sadd(s1,v11)
+        self.assertEqual(r.smembers(s1), set([sv11]))
+        self.assertEqual(b1.smembers(s1), set([sv11]))
+        self.assertEqual(b2.smembers(s1), set([sv11]))
+        
+        b1.sadd(s1,v12)
+        self.assertEqual(r.smembers(s1), set([sv11]))
+        self.assertEqual(b1.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b2.smembers(s1), set([sv11,sv12]))
+        
+        b2.sadd(s1,v13)
+        self.assertEqual(r.smembers(s1), set([sv11]))
+        self.assertEqual(b1.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b2.smembers(s1), set([sv11,sv12, sv13]))
+        
+        b2.srem(s1,v11)
+        self.assertEqual(r.smembers(s1), set([sv11]))
+        self.assertEqual(b1.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b2.smembers(s1), set([sv12, sv13]))
+
+        self.assertTrue(b1.sismember(s1,sv12))
+        self.assertFalse(r.sismember(s1,sv12))
+        self.assertFalse(b2.sismember(s1,sv11))
+        
+        b1.save()
+        self.assertEqual(r.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b1.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b2.smembers(s1), set([sv12, sv13]))
+        
+        b2.save()
+        self.assertEqual(r.smembers(s1), set([sv11,sv12]))
+        self.assertEqual(b1.smembers(s1), set([sv12, sv13]))
+        self.assertEqual(b2.smembers(s1), set([sv12, sv13]))
+        
+        b2.saveAll()
+        self.assertEqual(r.smembers(s1), set([sv12,sv13]))
+        self.assertEqual(b1.smembers(s1), set([sv12, sv13]))
+        self.assertEqual(b2.smembers(s1), set([sv12, sv13]))
+    
+    def test_list(self):
+        b1 = self.b1
+        b2 = self.b2
+        r = REDIS
+
+        s1 = 'test1'
+        v11 = 11
+        v12 = 12
+        v13 = 13
+        v14 = 14
+        v15 = 15
+        sv11 = str(v11)
+        sv12 = str(v12)
+        sv13 = str(v13)
+        sv14 = str(v14)
+        sv15 = str(v15)
+        
+        self.assertEqual(r.lrange(s1,0,-1), [])
+        self.assertEqual(b1.lrange(s1,0,-1), [])
+        self.assertEqual(b2.lrange(s1,0,-1), [])
+
+        r.lpush(s1,v11) 
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv11])
+        
+        b1.lpush(s1,v12) 
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv11])
+
+        b2.rpush(s1,v13) 
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv11,sv13])
+        self.assertEqual(r.llen(s1), 1)
+        self.assertEqual(b1.llen(s1), 2)
+        self.assertEqual(b2.llen(s1), 3)
+        self.assertEqual(r.lindex(s1,0), sv11)
+        self.assertEqual(b1.lindex(s1,1), sv11)
+        self.assertEqual(b2.lindex(s1,2), sv13)
+
+        b2.linsert(s1,'before',v11,v14)
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv11,sv13])
+        
+        b2.linsert(s1,'after',v11,v15)
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv11,sv15,sv13])
+
+        b2.rpush(s1,v11) 
+        b2.lpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.lrem(s1,0,v11)
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv15,sv13])
+        
+        b2.rpush(s1,v11) 
+        b2.lpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.lrem(s1,-2,v11)
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv11,sv12,sv14,sv15,sv13,sv11])
+        
+        b2.rpush(s1,v11) 
+        b2.lpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.rpush(s1,v11) 
+        b2.lrem(s1,3,v11)
+        self.assertEqual(r.lrange(s1,0,-1), [sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+
+        b1.save()
+        self.assertEqual(r.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+        
+        b2.save()
+        self.assertEqual(r.lrange(s1,0,-1), [sv12,sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+        
+
+        b2.saveAll()
+        self.assertEqual(r.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+        self.assertEqual(b1.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+        self.assertEqual(b2.lrange(s1,0,-1), [sv12,sv14,sv15,sv13,sv11,sv11,sv11])
+
+if __name__ == '__main__':
+    unittest.main()
