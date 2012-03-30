e5cdf04763f0c71a16e81d1bfe5bd7a7f198d410
Dimitri <watel.dimitri@gmail.com>
2012-03-30 13:00:33 +0200

    Test des méthodes concernant les racines des arbres d'actions, et refactoring pour avoir un code plus lisible (utilisation d'une classe interface de joueur)

--- foret_action.py
+++ foret_action.py
@@ -5,6 +5,88 @@ from joueurs import *
 REDIS = redis.StrictRedis()
 
 
+class JoueurNodeInterface:
+    ''' Classe qui représente juste l'interface entre le joueur et les noeuds'''
+
+    def __init__(self,num):
+        self.num = num
+    
+    
+    def __eq__(self,other):
+        return self.num == other.num
+    
+    def __ne__(self,other):
+        return self.num != other.num
+    
+    def __str__(self):
+        return str(self.num)
+
+    
+    def setFirstRoot(self,node):
+        REDIS.set('J'+str(self.num)+':racine',node.num)
+
+    def getFirstRoot(self):
+        return Node(int(REDIS.get('J'+str(self.num)+':racine')))
+
+    def hasRoot(self):
+        return REDIS.exists('J'+str(self.num)+':racine')
+
+    def setLastRoot(self,node):
+        REDIS.set('J'+str(self.num)+':derniereRacine',node.num)
+
+    def getLastRoot(self):
+        return Node(int(REDIS.get('J'+str(self.num)+':derniereRacine')))
+    
+    def setNewRoot(self):
+
+        node = Node.getNextNewNode()
+        hasRoot = self.hasRoot()
+        if hasRoot:
+            lastRoot = self.getLastRoot()
+            lastRoot.setNextRoot(node)
+            node.setPreviousRoot(lastRoot)
+        else:
+            self.setFirstRoot(node)
+            node.setPreviousRoot(NodeCst.NULL)
+        self.setLastRoot(node)
+        node.setNextRoot(NodeCst.NULL)
+        
+        node.setPlayer(self)
+        node.setRoot(node)
+        node.setNextNode(NodeCst.NULL)
+        node.setLastChildNode(NodeCst.NULL)
+        node.setSiblingNode(NodeCst.NULL)
+        node.setPSiblingNode(NodeCst.NULL)
+        node.setFatherNode(NodeCst.NULL)
+    
+        Node.incrNodeId()
+
+        return node
+
+    def setNewFirstRoot(self):
+
+        node = Node.getNextNewNode()
+        if self.hasRoot():
+            firstRoot = self.getFirstRoot()
+            node.setNextRoot(firstRoot)
+            firstRoot.setPreviousRoot(node)
+        else:
+            self.setLastRoot(node)
+            node.setNextRoot(NodeCst.NULL)
+        self.setFirstRoot(node)
+        node.setPreviousRoot(NodeCst.NULL)
+
+        node.setPlayer(self)
+        node.setRoot(node)
+        node.setNextNode(NodeCst.NULL)
+        node.setLastChildNode(NodeCst.NULL)
+        node.setSiblingNode(NodeCst.NULL)
+        node.setPSiblingNode(NodeCst.NULL)
+        node.setFatherNode(NodeCst.NULL)
+        Node.incrNodeId()
+        
+        return node
+
 class Node:
     ''' Classe représentant une forêt dont les arbres sont placés les uns à la suite.
     Chaque noeud contient un ensemble d'étiquettes qu'il enregistre. Puis un curseur
@@ -27,11 +109,11 @@ class Node:
 
 # Interface avec la base de données
     
-    def setPlayer(node, jnum):
-        REDIS.set('N'+str(node.num)+':joueur',jnum)
+    def setPlayer(node, j):
+        REDIS.set('N'+str(node.num)+':joueur',j.num)
 
     def getPlayer(node):
-        return int(REDIS.get('N'+str(node.num)+':joueur'))
+        return JoueurNodeInterface(int(REDIS.get('N'+str(node.num)+':joueur')))
 
     def setNextNode(fatherNode,childNode):
         REDIS.set('N'+str(fatherNode.num)+':next',childNode.num)
@@ -71,25 +153,6 @@ class Node:
 
     # Interface pour les racines
    
-    @staticmethod
-    def setFirstRoot(node,jnum):
-        REDIS.set('J'+str(jnum)+':racine',node.num)
-
-    @staticmethod
-    def getFirstRoot(jnum):
-        return Node(int(REDIS.get('J'+str(jnum)+':racine')))
-
-    @staticmethod
-    def hasRoot(jnum):
-        return REDIS.exists('J'+str(jnum)+':racine')
-
-    @staticmethod
-    def setLastRoot(node,jnum):
-        REDIS.set('J'+str(jnum)+':derniereRacine',node.num)
-
-    @staticmethod
-    def getLastRoot(jnum):
-        return Node(int(REDIS.get('J'+str(jnum)+':derniereRacine')))
     
     def setNextRoot(root,node):
         REDIS.set('N'+str(root.num)+':racineSuivante',node.num)
@@ -146,69 +209,13 @@ class Node:
 
 # Methode de travail avec des racines
 
-    @staticmethod
-    def setNewRoot(jnum):
+    def setNewNextRoot(root):
 
         node = Node.getNextNewNode()
-        hasRoot = Node.hasRoot(jnum)
-        if hasRoot:
-            lastRoot = Node.getLastRoot(jnum)
-            lastRoot.setNextRoot(node)
-            node.setPreviousRoot(lastRoot)
-        else:
-            Node.setFirstRoot(node,jnum)
-            node.setPreviousRoot(NodeCst.NULL)
-        Node.setLastRoot(node,jnum)
-        node.setNextRoot(NodeCst.NULL)
-        
-        node.setPlayer(jnum)
-        node.setRoot(node)
-        node.setNextNode(NodeCst.NULL)
-        node.setLastChildNode(NodeCst.NULL)
-        node.setSiblingNode(NodeCst.NULL)
-        node.setPSiblingNode(NodeCst.NULL)
-        node.setFatherNode(NodeCst.NULL)
-    
-        Node.incrNodeId()
-
-        if hasRoot: 
-            ls = lastRoot.getLeavesOf()
-            for n in ls:
-                n.setNextNode(n,node)
-        return node
-
-    @staticmethod
-    def setNewFirstRoot(j):
-
-        node = Node.getNextNewNode()
-        if Node.hasRoot(j):
-            firstRoot = Node.getFirstRoot(j)
-            node.setNextRoot(firstRoot)
-            firstRoot.setPreviousRoot(node)
-        else:
-            Node.setLastRoot(node,j)
-            node.setNextRoot(NodeCst.NULL)
-        node.setFirstRoot(j)
-        node.setPreviousRoot(NodeCst.NULL)
-
-        Node.setPlayer(node,j)
-        node.setRoot(node)
-        node.setNextNode(NodeCst.NULL)
-        node.setLastChildNode(NodeCst.NULL)
-        node.setSiblingNode(NodeCst.NULL)
-        node.setPSiblingNode(NodeCst.NULL)
-        node.setFatherNode(NodeCst.NULL)
-        Node.incrNodeId()
-        
-        return node
-    
-    @staticmethod
-    def setNewNextRoot(root, j):
-
-        node = Node.getNextNewNode()
-        lastRoot = Node.getLastRoot(j)
+        j = root.getPlayer()
+        lastRoot = j.getLastRoot()
         if lastRoot == root:
-            Node.setLastRoot(node,j)
+            j.setLastRoot(node)
             node.setNextRoot(NodeCst.NULL)
         nroot = root.getNextRoot()
         node.setNextRoot(nroot)
@@ -217,7 +224,7 @@ class Node:
         root.setNextRoot(node)
         node.setPreviousRoot(root)
 
-        Node.setPlayer(node,j)
+        node.setPlayer(j)
         node.setRoot(node)
         node.setNextNode(NodeCst.NULL)
         node.setLastChildNode(NodeCst.NULL)
@@ -226,31 +233,23 @@ class Node:
         node.setFatherNode(NodeCst.NULL)
         Node.incrNodeId()
 
-
-        ls = Node.getLeavesOf(root)
-        for n in ls:
-            n.setNextNode(node)
         return node
    
-    @staticmethod
     def removeRoot(root):
         j = root.getPlayer()
-        froot = Node.getFirstRoot(j)
-        lroot = Node.getLastRoot(j)
+        froot = j.getFirstRoot()
+        lroot = j.getLastRoot()
         nroot = root.getNextRoot()
         proot = root.getPreviousRoot()
 
         if proot != NodeCst.NULL:
             proot.setNextRoot(nroot) 
-            ls = proot.getLeavesOf()
-            for n in ls:
-                n.setNextNode(nroot)
         if nroot != NodeCst.NULL:
             nroot.setPreviousRoot(proot)
         if root == froot:
-            Node.setFirstRoot(nroot,j)
+            j.setFirstRoot(nroot)
         if root == lroot:
-            Node.setLastRoot(proot,j)
+            j.setLastRoot(proot)
  
 
         l = Node.selectNodesFromRoot(root)
@@ -306,6 +305,7 @@ class Node:
         lastNode.setFatherNode(fatherNode)
         lastNode.setLastChildNode(NodeCst.NULL)
         lastNode.setSiblingNode(NodeCst.NULL)
+        lastNode.setNextNode(NodeCst.NULL)
 
         Node.incrNodeId()
 
@@ -313,8 +313,6 @@ class Node:
         lastNode.setRoot(root)
         lastNode.setPlayer(fatherNode.getPlayer())
         
-        nextRoot = root.getNextRoot()
-        lastNode.setNextNode(nextRoot)
 
         return lastNode
 
@@ -339,8 +337,6 @@ class Node:
         lastNode.setRoot(root)
         lastNode.setPlayer(fatherNode.getPlayer())
         
-        nextRoot = root.getNextRoot()
-        lastNode.setNextNode(nextRoot)
         return lastNode
         
 
@@ -368,8 +364,6 @@ class Node:
         lastNode.setRoot(root)
         lastNode.setPlayer(node.getPlayer())
         
-        nextRoot = root.getNextRoot()
-        lastNode.setNextNode(nextRoot)
         return lastNode
     
     def removeNode(node):
@@ -411,11 +405,7 @@ class Node:
             if(len(childs) != 0):
                 fatherNode.setNextNode(childs[0])
             else:
-                if siblingNode != NodeCst.NULL:
-                    fatherNode.setNextNode(siblingNode)
-                else:
-                    nRoot = fatherNode.getRoot().getNextRoot()
-                    fatherNode.setNextNode(nRoot)
+                fatherNode.setNextNode(siblingNode)
 
         node.deleteNode()
 
@@ -456,6 +446,33 @@ class Node:
         ''' Retire l'action du noeud node '''
         REDIS.lrem('N'+str(node.num)+':actions',0,action.num)
         Action.delAction(action.num)
+
+# Execution
+
+#    def peutExecuterAction(self, action):
+#        ''' Vérifie si on peut executer l'action, avec la base de donnée bdd '''
+#        func = getattr(Jeu, 'peut_'+action.func)
+#        if func.peut_etre_appelee:
+#            return Jeu.func(self,*action.paramsi)
+#        return False
+
+#    def executerAction(self, action):
+#        ''' Execute l'action, avec la base de donnée bdd '''
+#        func = getattr(Jeu, action.func)
+#        if func.peut_etre_appelee:
+#            return Jeu.func(self,*action.params)
+#        return False
+
+#    def executerNode(self):
+#        ''' Execute l'ensemble des actions du noeud jusqu' à la dernière ou jusqu'a ce qu'une des action renvoie faux. Dans le premire cas, elle renvoie vrai et dans l'autre, vide la  base de données et renvoie faux.'''
+#        actions = ForetAction.getActions(node)
+#        for actnum in actions:
+#            action = Action.getAction(int(actnum))
+#            b = self.executerAction(action)
+#            if not b:
+#                self.bdd.flushSurface()
+#                return False
+#        return True
         
 
 class Action:
@@ -521,30 +538,6 @@ if __name__ == '__main__':
 #        ''' Ajoute un arbre d'action à la foret de ce joueur, juste après l'arbre de racine root '''
 #        return ForetAction.setNewNextRoot(root,self) 
     
-#    def peutExecuterAction(self, action):
-#        ''' Vérifie si on peut executer l'action, avec la base de donnée bdd '''
-#        func = getattr(Jeu, 'peut_'+action.func)
-#        if func.peut_etre_appelee:
-#            return Jeu.func(self,*action.paramsi)
-#        return False
-
-#    def executerAction(self, action):
-#        ''' Execute l'action, avec la base de donnée bdd '''
-#        func = getattr(Jeu, action.func)
-#        if func.peut_etre_appelee:
-#            return Jeu.func(self,*action.params)
-#        return False
-
-#    def executerNode(self, node):
-#        ''' Execute l'ensemble des actions du noeud jusqu' à la dernière ou jusqu'a ce qu'une des action renvoie faux. Dans le premire cas, elle renvoie vrai et dans l'autre, vide la  base de données et renvoie faux.'''
-#        actions = ForetAction.getActions(node)
-#        for actnum in actions:
-#            action = Action.getAction(int(actnum))
-#            b = self.executerAction(action)
-#            if not b:
-#                self.bdd.flushSurface()
-#                return False
-#        return True
         
 #    def executerForetAction(self):
 #        ''' Execute l'ensemble de la foret d'action et renvoie la dernière base de données avant que l'execution ne s'arrete. '''
--- test_foret_action.py
+++ test_foret_action.py
@@ -9,19 +9,19 @@ class TestForetAction(unittest.TestCase):
 
     def setUp(self):
         REDIS.flushdb()
-        self.j1 = 1
+        self.j1 = JoueurNodeInterface(1)
 
     def test_first_root(self):
-        n = Node.setNewRoot(self.j1)
-        self.assertEqual(Node.getFirstRoot(self.j1),n)
-        self.assertEqual(Node.getLastRoot(self.j1),n)
+        n =self.j1.setNewRoot()
+        self.assertEqual(self.j1.getFirstRoot(),n)
+        self.assertEqual(self.j1.getLastRoot(),n)
         nu = NodeCst.NULL
         self.assert_node(n,nu,nu,nu,nu,nu,n,self.j1)
         self.assertEqual(n.getNextRoot(),nu)
         self.assertEqual(n.getPreviousRoot(),nu)
 
     def test_add_child(self):
-        n = Node.setNewRoot(self.j1)
+        n =self.j1.setNewRoot()
         nu = NodeCst.NULL
 
         n1 = n.addChild()
@@ -58,7 +58,7 @@ class TestForetAction(unittest.TestCase):
         self.assert_node(n11,nu,n8,nu,n10,nu,n,self.j1)
 
     def test_add_first_child(self):
-        n = Node.setNewRoot(self.j1)
+        n =self.j1.setNewRoot()
         nu = NodeCst.NULL
 
         n3 = n.addFirstChild()
@@ -98,7 +98,7 @@ class TestForetAction(unittest.TestCase):
         self.assert_node(n11,nu,n8,nu,n10,nu,n,self.j1)
 
     def test_add_sibling(self):
-        n = Node.setNewRoot(self.j1)
+        n =self.j1.setNewRoot()
         nu = NodeCst.NULL
 
         n1 = n.addChild()
@@ -137,7 +137,7 @@ class TestForetAction(unittest.TestCase):
         self.assert_node(n12,nu,n8,nu,n11,nu,n,self.j1)
     
     def test_remove(self):
-        n1 = Node.setNewRoot(self.j1)
+        n =n1 = self.j1.setNewRoot()
         nu = NodeCst.NULL
 
         n2 = n1.addChild()
@@ -251,6 +251,221 @@ class TestForetAction(unittest.TestCase):
         self.assert_node(n12,nu,n6,nu,n10,nu,n1,self.j1)
         self.assert_node(n13,nu,n1,n14,nu,nu,n1,self.j1)
         self.assert_node(n14,nu,n1,n5,n13,nu,n1,self.j1)
+
+    def test_new_root(self):
+        n = self.j1.setNewRoot()
+        nu = NodeCst.NULL
+
+        n4 = n.addChild()
+        n2 = n.addFirstChild()
+        n3 = n2.addSibling()
+        
+        n9 = n3.addChild()
+        n7 = n3.addFirstChild()
+        n8 = n7.addSibling()
+
+        n13 = self.j1.setNewRoot()
+        
+        n6 = n.addChild()
+        n1 = n.addFirstChild()
+        n5 = n4.addSibling()
+        
+        n12 = n4.addChild()
+        n10 = n4.addFirstChild()
+        n11 = n10.addSibling()
+        
+        n14 = n10.addChild()
+        n14.removeNode()
+        n15 = self.j1.setNewFirstRoot()
+        n16 = self.j1.setNewRoot()
+        
+        self.assert_node(n15,nu,nu,nu,nu,nu,n15,self.j1)
+        self.assertEqual(n15.getNextRoot(),n)
+        self.assertEqual(n15.getPreviousRoot(),nu)
+        
+        self.assert_node(n,n1,nu,nu,nu,n6,n,self.j1)
+        self.assertEqual(n.getNextRoot(),n13)
+        self.assertEqual(n.getPreviousRoot(),n15)
+
+        self.assert_node(n1,nu,n,n2,nu,nu,n,self.j1)
+        self.assert_node(n2,nu,n,n3,n1,nu,n,self.j1)
+        self.assert_node(n3,n7,n,n4,n2,n9,n,self.j1)
+        self.assert_node(n4,n10,n,n5,n3,n12,n,self.j1)
+        self.assert_node(n5,nu,n,n6,n4,nu,n,self.j1)
+        self.assert_node(n6,nu,n,nu,n5,nu,n,self.j1)
+        self.assert_node(n7,nu,n3,n8,nu,nu,n,self.j1)
+        self.assert_node(n8,nu,n3,n9,n7,nu,n,self.j1)
+        self.assert_node(n9,nu,n3,nu,n8,nu,n,self.j1)
+        self.assert_node(n10,nu,n4,n11,nu,nu,n,self.j1)
+        self.assert_node(n11,nu,n4,n12,n10,nu,n,self.j1)
+        self.assert_node(n12,nu,n4,nu,n11,nu,n,self.j1)
+       
+        self.assert_node(n13,nu,nu,nu,nu,nu,n13,self.j1)
+        self.assertEqual(n13.getNextRoot(),n16)
+        self.assertEqual(n13.getPreviousRoot(),n)
+        
+        self.assert_node(n16,nu,nu,nu,nu,nu,n16,self.j1)
+        self.assertEqual(n16.getNextRoot(),nu)
+        self.assertEqual(n16.getPreviousRoot(),n13)
+    
+    def test_new_next_root(self):
+        n = self.j1.setNewRoot()
+        nu = NodeCst.NULL
+
+        n4 = n.addChild()
+        n2 = n.addFirstChild()
+        n3 = n2.addSibling()
+        
+        n9 = n3.addChild()
+        n7 = n3.addFirstChild()
+        n8 = n7.addSibling()
+
+        n13 = n.setNewNextRoot()
+        
+        n6 = n.addChild()
+        n1 = n.addFirstChild()
+        n5 = n4.addSibling()
+        
+        n12 = n4.addChild()
+        n10 = n4.addFirstChild()
+        n11 = n10.addSibling()
+        
+        n14 = n10.addChild()
+        n14.removeNode()
+        n15 = self.j1.setNewFirstRoot()
+        n16 = self.j1.setNewRoot()
+        
+        self.assert_node(n15,nu,nu,nu,nu,nu,n15,self.j1)
+        self.assertEqual(n15.getNextRoot(),n)
+        self.assertEqual(n15.getPreviousRoot(),nu)
+        
+        self.assert_node(n,n1,nu,nu,nu,n6,n,self.j1)
+        self.assertEqual(n.getNextRoot(),n13)
+        self.assertEqual(n.getPreviousRoot(),n15)
+
+        self.assert_node(n1,nu,n,n2,nu,nu,n,self.j1)
+        self.assert_node(n2,nu,n,n3,n1,nu,n,self.j1)
+        self.assert_node(n3,n7,n,n4,n2,n9,n,self.j1)
+        self.assert_node(n4,n10,n,n5,n3,n12,n,self.j1)
+        self.assert_node(n5,nu,n,n6,n4,nu,n,self.j1)
+        self.assert_node(n6,nu,n,nu,n5,nu,n,self.j1)
+        self.assert_node(n7,nu,n3,n8,nu,nu,n,self.j1)
+        self.assert_node(n8,nu,n3,n9,n7,nu,n,self.j1)
+        self.assert_node(n9,nu,n3,nu,n8,nu,n,self.j1)
+        self.assert_node(n10,nu,n4,n11,nu,nu,n,self.j1)
+        self.assert_node(n11,nu,n4,n12,n10,nu,n,self.j1)
+        self.assert_node(n12,nu,n4,nu,n11,nu,n,self.j1)
+       
+        self.assert_node(n13,nu,nu,nu,nu,nu,n13,self.j1)
+        self.assertEqual(n13.getNextRoot(),n16)
+        self.assertEqual(n13.getPreviousRoot(),n)
+        
+        self.assert_node(n16,nu,nu,nu,nu,nu,n16,self.j1)
+        self.assertEqual(n16.getNextRoot(),nu)
+        self.assertEqual(n16.getPreviousRoot(),n13)
+        
+    def test_remove_root(self):
+        n = self.j1.setNewRoot()
+        nu = NodeCst.NULL
+
+        n4 = n.addChild()
+        n2 = n.addFirstChild()
+        n3 = n2.addSibling()
+        
+        n9 = n3.addChild()
+        n7 = n3.addFirstChild()
+        n8 = n7.addSibling()
+
+        n13 = n.setNewNextRoot()
+        
+        n6 = n.addChild()
+        n1 = n.addFirstChild()
+        n5 = n4.addSibling()
+        
+        n12 = n4.addChild()
+        n10 = n4.addFirstChild()
+        n11 = n10.addSibling()
+        
+        n14 = n10.addChild()
+        n14.removeNode()
+        n15 = self.j1.setNewFirstRoot()
+        n16 = self.j1.setNewRoot()
+    
+        n17 = n13.addChild()
+        n18 = n13.addFirstChild()
+        n19 = n17.addSibling()
+        n20 = n17.addChild()
+
+        n13.removeRoot()
+    
+        self.assert_node(n15,nu,nu,nu,nu,nu,n15,self.j1)
+        self.assertEqual(n15.getNextRoot(),n)
+        self.assertEqual(n15.getPreviousRoot(),nu)
+        
+        self.assert_node(n,n1,nu,nu,nu,n6,n,self.j1)
+        self.assertEqual(n.getNextRoot(),n16)
+        self.assertEqual(n.getPreviousRoot(),n15)
+
+        self.assert_node(n1,nu,n,n2,nu,nu,n,self.j1)
+        self.assert_node(n2,nu,n,n3,n1,nu,n,self.j1)
+        self.assert_node(n3,n7,n,n4,n2,n9,n,self.j1)
+        self.assert_node(n4,n10,n,n5,n3,n12,n,self.j1)
+        self.assert_node(n5,nu,n,n6,n4,nu,n,self.j1)
+        self.assert_node(n6,nu,n,nu,n5,nu,n,self.j1)
+        self.assert_node(n7,nu,n3,n8,nu,nu,n,self.j1)
+        self.assert_node(n8,nu,n3,n9,n7,nu,n,self.j1)
+        self.assert_node(n9,nu,n3,nu,n8,nu,n,self.j1)
+        self.assert_node(n10,nu,n4,n11,nu,nu,n,self.j1)
+        self.assert_node(n11,nu,n4,n12,n10,nu,n,self.j1)
+        self.assert_node(n12,nu,n4,nu,n11,nu,n,self.j1)
+        
+        self.assert_node(n16,nu,nu,nu,nu,nu,n16,self.j1)
+        self.assertEqual(n16.getNextRoot(),nu)
+        self.assertEqual(n16.getPreviousRoot(),n)
+        
+        n16.removeRoot()
+        
+        self.assert_node(n15,nu,nu,nu,nu,nu,n15,self.j1)
+        self.assertEqual(n15.getNextRoot(),n)
+        self.assertEqual(n15.getPreviousRoot(),nu)
+        
+        self.assert_node(n,n1,nu,nu,nu,n6,n,self.j1)
+        self.assertEqual(n.getNextRoot(),nu)
+        self.assertEqual(n.getPreviousRoot(),n15)
+
+        self.assert_node(n1,nu,n,n2,nu,nu,n,self.j1)
+        self.assert_node(n2,nu,n,n3,n1,nu,n,self.j1)
+        self.assert_node(n3,n7,n,n4,n2,n9,n,self.j1)
+        self.assert_node(n4,n10,n,n5,n3,n12,n,self.j1)
+        self.assert_node(n5,nu,n,n6,n4,nu,n,self.j1)
+        self.assert_node(n6,nu,n,nu,n5,nu,n,self.j1)
+        self.assert_node(n7,nu,n3,n8,nu,nu,n,self.j1)
+        self.assert_node(n8,nu,n3,n9,n7,nu,n,self.j1)
+        self.assert_node(n9,nu,n3,nu,n8,nu,n,self.j1)
+        self.assert_node(n10,nu,n4,n11,nu,nu,n,self.j1)
+        self.assert_node(n11,nu,n4,n12,n10,nu,n,self.j1)
+        self.assert_node(n12,nu,n4,nu,n11,nu,n,self.j1)
+        
+
+        n15.removeRoot()
+        
+        self.assert_node(n,n1,nu,nu,nu,n6,n,self.j1)
+        self.assertEqual(n.getNextRoot(),nu)
+        self.assertEqual(n.getPreviousRoot(),nu)
+
+        self.assert_node(n1,nu,n,n2,nu,nu,n,self.j1)
+        self.assert_node(n2,nu,n,n3,n1,nu,n,self.j1)
+        self.assert_node(n3,n7,n,n4,n2,n9,n,self.j1)
+        self.assert_node(n4,n10,n,n5,n3,n12,n,self.j1)
+        self.assert_node(n5,nu,n,n6,n4,nu,n,self.j1)
+        self.assert_node(n6,nu,n,nu,n5,nu,n,self.j1)
+        self.assert_node(n7,nu,n3,n8,nu,nu,n,self.j1)
+        self.assert_node(n8,nu,n3,n9,n7,nu,n,self.j1)
+        self.assert_node(n9,nu,n3,nu,n8,nu,n,self.j1)
+        self.assert_node(n10,nu,n4,n11,nu,nu,n,self.j1)
+        self.assert_node(n11,nu,n4,n12,n10,nu,n,self.j1)
+        self.assert_node(n12,nu,n4,nu,n11,nu,n,self.j1)
+
 
     def assert_node(self, n, nN, fN, sN, pSN, lCN, r, p):
         ''' Vérifie si pour le noeud n, son premier fils est nN, son pere est fN, son frere droit est sN, son frere gauche est pSN, sont dernier fils est lCN, sa racine est r et son joueuer est p'''
