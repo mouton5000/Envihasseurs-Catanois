e0c437b30195441db303c495e742360f22599b35
Dimitri <watel.dimitri@gmail.com>
2012-06-01 17:09:06 +0200

    Script permettant de tester sur le client des données utilisées apr le controlleur

--- dbTest.py
+++ dbTest.py
@@ -14,7 +14,17 @@ tg = p.ter(1)
 td = p.ter(2)
 
 Colonie(1,p.it(62)).save()
+Colonie(2,p.it(64)).save()
+c = Colonie(1,p.it(44))
+c.evolue()
+c.save()
 Route(1,p.it(62).lien(p.it(72))).save()
+Route(2,p.it(58).lien(p.it(59))).save()
+Bateau(1,1,p.it(56).lien(p.it(57)),Cartes.RIEN,Bateau.BateauType.TRANSPORT,False).save()
+Bateau(2,1,p.it(101).lien(p.it(102)),Cartes.RIEN,Bateau.BateauType.CARGO,False).save()
+Bateau(3,1,p.it(7).lien(p.it(8)),Cartes.RIEN,Bateau.BateauType.VOILIER,False).save()
+Bateau(4,2,p.it(90).lien(p.it(100)),Cartes.RIEN,Bateau.BateauType.CARGO,False).save()
+
 
 cg = Tarifs.ROUTE*4 + Tarifs.COLONIE*2
 cd = Tarifs.ROUTE*2
@@ -42,3 +52,10 @@ act2.save()
 
 n2.addAction(act1)
 n3.addAction(act2)
+        
+
+Voleur(p.hexa(22),Voleur.VoleurType.BRIGAND,p.ter(1)).save(REDIS)
+Voleur(p.hexa(1),Voleur.VoleurType.PIRATE,p.ter(1)).save(REDIS)
+
+Voleur(p.hexa(25),Voleur.VoleurType.BRIGAND,p.ter(2)).save(REDIS)
+Voleur(p.hexa(4),Voleur.VoleurType.PIRATE,p.ter(2)).save(REDIS)
